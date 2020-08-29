4aa2665cbd980709a9512b3f210e5355f747adb7
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-08-29 12:14:11 -0700

    Starting the intial syntax of PHP

---
+++ syntax.php
@@ -0,0 +1,16 @@
+<!-- Basic Syntax <?php code ?> -->
+
+<?php
+    echo "Hello World";
+    echo "<h2> Hello Second World </h2>";
+?>
+
+<html>
+<body>
+
+<?php 
+    echo "<h1>Hello Earth!</h1>";
+?>
+
+</body>
+</html>
